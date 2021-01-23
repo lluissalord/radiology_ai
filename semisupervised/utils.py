08c8706739b5f583de1e6e14a7c74af122606eb1
Lluis <l.salord.quetglas@gmail.com>
2021-01-23 14:15:37 +0100

    Fix interleave

--- semisupervised/utils.py
+++ semisupervised/utils.py
@@ -2,13 +2,18 @@
 
 import torch
 import pandas as pd
+import numpy as np
 
 def interleave(x, bs):
     """ Interleave to mix batch data """
     s = list(x.shape)
-    return torch.reshape(torch.transpose(x.reshape([-1, bs] + s[1:]), 1, 0), [-1] + s[1:])
+    idx = np.argmax((np.array(s) != 1)) + 1
+    ret = torch.reshape(torch.transpose(x.reshape([-1, bs] + s[idx:]), 1, 0), s)
+    return ret
 
 def de_interleave(x, bs):
     """ Deinterleave to undo mix of batch data """
     s = list(x.shape)
-    return torch.reshape(torch.transpose(x.reshape([bs, -1] + s[1:]), 1, 0), [-1] + s[1:])
+    idx = np.argmax((np.array(s) != 1)) + 1
+    ret = torch.reshape(torch.transpose(x.reshape([bs, -1] + s[idx:]), 1, 0), s)
+    return ret
