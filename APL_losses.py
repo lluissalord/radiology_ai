a6d96bbfa9e9642be329d700626a53dc7c103d07
Lluis <l.salord.quetglas@gmail.com>
2021-03-08 17:24:26 +0100

    Rewrite FocalLoss

--- APL_losses.py
+++ APL_losses.py
@@ -12,8 +12,11 @@ Extracted [Normalized Loss Functions for Deep Learning with Noisy Labels](https:
 """
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+
+from fastai.losses import BaseLoss
 
 if torch.cuda.is_available():
     torch.backends.cudnn.benchmark = True
@@ -317,44 +320,33 @@ class NLNL(FastaiLoss):
         return loss
 
 
-class FocalLoss(FastaiLoss):
-    '''
-        https://github.com/clcarwin/focal_loss_pytorch/blob/master/focalloss.py
-    '''
+class FocalLoss(nn.CrossEntropyLoss):
+    ''' Focal loss for classification tasks on imbalanced datasets '''
 
-    def __init__(self, gamma=0, alpha=None, size_average=True):
-        super(FocalLoss, self).__init__(num_classes=num_classes)
+    def __init__(self, gamma, weight=None, ignore_index=-100, reduction='mean'):
+        super().__init__(weight=weight, ignore_index=ignore_index, reduction='none')
+        self.reduction = reduction
         self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha, (float, int)):
-            self.alpha = torch.Tensor([alpha, 1-alpha])
-        if isinstance(alpha, list):
-            self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
 
-    def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1, 2)                         # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1, input.size(2))    # N,H*W,C => N*H*W,C
-        target = target.view(-1, 1)
+    def forward(self, input_, target):
+        cross_entropy = super().forward(input_, target)
+        # Temporarily mask out ignore index to '0' for valid gather-indices input.
+        # This won't contribute final loss as the cross_entropy contribution
+        # for these would be zero.
+        target = target * (target != self.ignore_index).long()
+        input_prob = torch.gather(F.softmax(input_, 1), 1, target.unsqueeze(1))
+        loss = torch.pow(1 - input_prob, self.gamma) * cross_entropy
+        return torch.mean(loss) if self.reduction == 'mean' else torch.sum(loss) if self.reduction == 'sum' else loss
 
-        logpt = F.log_softmax(input, dim=1)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = torch.autograd.Variable(logpt.data.exp())
 
-        if self.alpha is not None:
-            if self.alpha.type() != input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0, target.data.view(-1))
-            logpt = logpt * torch.autograd.Variable(at)
-
-        loss = -1 * (1-pt)**self.gamma * logpt
-        if self.size_average:
-            return loss.mean()
-        else:
-            return loss.sum()
+class FocalLossFlat(BaseLoss):
+    "Same as `FocalLoss`, but flattens input and target."
+    y_int = True
+    def __init__(self, weight=None, ignore_index=-100, reduction='mean', *args, axis=-1, **kwargs): 
+        super().__init__(FocalLoss, weight=weight, ignore_index=ignore_index, reduction=reduction, *args, axis=axis, **kwargs)
+        self.axis = axis
+    def decodes(self, x):    return x.argmax(dim=self.axis)
+    def activation(self, x): return F.softmax(x, dim=self.axis)
 
 
 class NormalizedFocalLoss(FastaiLoss):
